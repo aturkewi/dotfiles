48aa7822d1e485d7ba1dc7878c88bb232c2759d5
avi turkewitz <aviturkewitz@gmail.com>
2019-10-06 18:22:02 -0400

    update the setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# check if each tool is installed _before_ installing it
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -7,3 +8,7 @@ brew install git bash-completion
 brew install asdf
 brew install direnv
 brew install gnupg
+brew install hub
+
+# create dir for screenshots
+# set dir for screenshots
